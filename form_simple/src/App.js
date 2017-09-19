e77ea5665e4806d5eb2fce29a6ca48c25fcc54b3
A-SK <eisuke_nakanishi@12knox.com>
2017-09-19 13:57:21 +0900

    Adding new lines for SimpleForm.

--- form_simple/src/App.js
+++ form_simple/src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { SimpleForm } from './SimpleForm';
 import './App.css';
 
-class App extends Component {
+export default class App extends Component {
   render() {
     return(
       <div className='App'>
