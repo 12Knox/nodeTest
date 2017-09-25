8afb2ce7f70c2953e8bfb6e2f9a6c11de2c0ecef
A-SK <eisuke_nakanishi@12knox.com>
2017-09-25 17:34:33 +0900

    Editing and processing the prj.

--- express_test/dice-q.js
+++ express_test/dice-q.js
@@ -5,10 +5,18 @@ const portNum = 3000;
 app.get('/', (req, res) => {
   if (!req.query.q) {
     res.send(
-      '<p><a href="/dice/6">6面体のサイコロ</a><br />' +
-      '<a href="/dice/12">12面体のサイコロ</a></p>');
+      '<p><a href="?q=6">6面体のサイコロ</a><br />' +
+      '<a href="?q=12">12面体のサイコロ</a></p>');
   } else {
     const q = parseInt(req.query.q, 10);
+    res.send('今回の値は...' + dice(q));
   }
 });
 
+function dice(n) {
+  return Math.floor(Math.random() * n) + 1;
+}
+
+app.listen(portNum, () => {
+  console.log('起動しました：' + `http:localhost:${portNum}`);
+});
